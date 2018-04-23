6f89da4bd48529d64482fb9f62ae7895f471d3f7
James Riall <james.riall@phntms.com>
2018-04-23 20:18:11 +0100

    Begin rewrite of game board

--- src/js/main.js
+++ src/js/main.js
@@ -13,102 +13,50 @@ const EventType = {
   LOAD: 'load',
 }
 
+const ClassName = {
+  ROW: 'row',
+}
+
+const STARTING_BOARD = [
+  [1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 2, 0, 2, 0, 2, 0, 2],
+  [2, 0, 2, 0, 2, 0, 2, 0],
+  [0, 2, 0, 2, 0, 2, 0, 2],
+]
+
 class App {
   constructor() {
-    this.pieces = [...document.getElementsByClassName('piece')];
-    this.redPieces = [...document.getElementsByClassName('red-piece')];
-    this.blackPieces = [...document.getElementsByClassName('black-piece')];
-    this.gameTiles = [...document.getElementsByClassName('game-tile')]
-    this.redScoreView = document.getElementById('red-score');
-    this.blackScoreView = document.getElementById('black-score');
-    this.redScore = 0;
-    this.blackScore = 0;
-    this.pieces.forEach(el => {
-      el.addEventListener(EventType.CLICK, () => {
-        this.handleSelect(el);
+    this.currentBoard = STARTING_BOARD;
+    this.rows = [...document.getElementsByClassName(ClassName.ROW)];
+    this.drawBoard();
+  }
+
+  drawBoard() {
+    this.rows.forEach((a, rowIndex) => {
+      [...a.childNodes]
+        .filter(a => a.tagName === 'TD')
+        .forEach((a, colIndex) => {
+          if (this.currentBoard[rowIndex][colIndex] === 1) {
+            a.innerHTML = this.renderPiece('black');
+          }
+          if (this.currentBoard[rowIndex][colIndex] === 2) {
+            a.innerHTML = this.renderPiece('red');
+          }
+        })
       });
-    });
-    this.gameTiles.forEach(el => {
-      el.addEventListener(EventType.CLICK, (a) => {
-        this.movePiece(this.selected, a);
-      })
-    });
-    this.canMove();
-    this.selected = ''
   }
 
-  isEmpty(tile) {
-    if (tile.target.children.length > 0 || tile.target.tagName === 'IMG') {
-      return false;
-    } else {
-      return true;
-    }
+  renderPiece(color) {
+    return `<img class="piece ${color}-piece"
+        src="/images/checkers-checker-${color}.svg"
+        alt="${color} piece">`;
   }
 
-  movePiece(piece, newLocation) {
-    console.log(newLocation.target);
-    if (this.isEmpty(newLocation)) {
-      const oldLocation = document.getElementById(piece);
-      oldLocation.parentNode.removeChild(oldLocation);
-      const color = piece.split('-')[0];
-      newLocation.target.innerHTML =
-          `<img
-              class="piece ${color}-piece"
-              src="./images/checkers-checker-${color}.svg"
-              alt="${color.charAt(0).toUpperCase() + color.slice(1)} piece"
-              id="${piece}">
-          `
-    }
-  }
 
-  handleSelect(el) {
-      if ([...el.classList].includes(Modifier.SELECTED)) {
-        el.classList.add(Modifier.MOVABLE);
-        el.classList.remove(Modifier.SELECTED);
-        this.selected = '';
-      } else {
-        this.pieces.forEach(a => a.classList.remove(Modifier.SELECTED));
-        el.classList.add(Modifier.SELECTED);
-        el.classList.remove(Modifier.MOVABLE);
-        this.selected = el.id;
-      }
-  }
-
-  canMove(playerTurn) {
-    // Highlights in white which pieces have available moves.
-    if (playerTurn === Player.RED) {
-
-    } else {
-
-    }
-  }
-
-  increaseScore(scoreToIncrease, num) {
-    if (scoreToIncrease = Player.RED) {
-      this.redScore += num;
-      this.redScoreView.innerHTML = this.redScore;
-      if (this.redScore === 12) {
-        this.handleVictory(Player.RED)
-      }
-    } else {
-      this.blackScore += num;
-      this.blackScoreView.innerHTML = this.blackScore;
-      if (this.blackScore === 12) {
-        this.handleVictory(Player.BLACK)
-      }
-    }
-  }
-
-  resetGame() {
-    this.redScore = 0;
-    this.blackScore = 0;
-    this.redScoreView.innerHTML = 0;
-    this.blackScoreView.innerHTML = 0;
-  }
-
-  handleVictory(winner) {
-
-  }
 }
 
 window.addEventListener(EventType.LOAD, () => new App());
